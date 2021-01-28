08a782e970cab1bc9c5c5028dbce0016117be610
Grishutenko Pavel <grishutenkopp@gmail.com>
2021-01-28 15:27:13 +0300

    dive into QT

---
+++ eduQT/1/main.cpp
@@ -0,0 +1,9 @@
+#include <QtWidgets>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QLabel lbl1("This is test QT app and start education");
+    lbl1.show();
+    return app.exec();
+}
