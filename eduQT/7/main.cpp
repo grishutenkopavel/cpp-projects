1a7d820e11253463f661d4e4aa430f58388ba2dd
Grishutenko Pavel <grishutenkopp@gmail.com>
2021-03-09 22:06:01 +0300

    scroll bars

---
+++ eduQT/7/main.cpp
@@ -0,0 +1,21 @@
+#include <QtWidgets>
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    QScrollArea sa;
+    
+    QWidget* pwgt = new QWidget;
+    QPixmap pix(":/stone.jpg");
+
+    QPalette pal;
+    pal.setBrush(pwgt->backgroundRole(), QBrush(pix));
+    pwgt->setPalette(pal);
+    pwgt->setAutoFillBackground(true);
+    pwgt->setFixedSize(pix.width(), pix.height());
+
+    sa.setWidget(pwgt);
+    sa.resize(350, 150);
+    sa.show();
+    return app.exec();
+}
