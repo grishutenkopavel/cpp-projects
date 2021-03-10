380474c790700a446838d4c161979adaeefd958f
Grishutenko Pavel <grishutenkopp@gmail.com>
2021-03-10 12:46:27 +0300

    stretching

---
+++ eduQT/9/main.cpp
@@ -0,0 +1,21 @@
+#include <QtWidgets>
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    QWidget wgt;
+
+    QPushButton* pcmdA = new QPushButton("A");
+    QPushButton* pcmdB = new QPushButton("B");
+
+    QBoxLayout* pbxLayout = new QBoxLayout(QBoxLayout::LeftToRight);
+    pbxLayout->addWidget(pcmdA);
+    pbxLayout->addStretch(2);
+    pbxLayout->addWidget(pcmdB);
+    wgt.setLayout(pbxLayout);
+
+    wgt.resize(350, 40);
+    wgt.show();
+    
+    return app.exec();
+}
